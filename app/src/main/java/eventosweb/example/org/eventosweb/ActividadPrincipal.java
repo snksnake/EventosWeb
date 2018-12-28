909791ead32fd4083cabcbb9465f0710a6b3e3e6
Alberto <snk_snake@hotmail.com>
2018-12-28 15:08:17 +0100

    Initial commit

---
+++ app/src/main/java/eventosweb/example/org/eventosweb/ActividadPrincipal.java
@@ -0,0 +1,13 @@
+package eventosweb.example.org.eventosweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+    }
+}

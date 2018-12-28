7c8b54a31fad590b3fe82ab826bb0826afbe30a8
Alberto <snk_snake@hotmail.com>
2018-12-28 15:12:38 +0100

    Abrir contenido web en línea en un WebView

--- app/src/main/java/eventosweb/example/org/eventosweb/ActividadPrincipal.java
+++ app/src/main/java/eventosweb/example/org/eventosweb/ActividadPrincipal.java
@@ -1,13 +1,21 @@
 package eventosweb.example.org.eventosweb;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActividadPrincipal extends AppCompatActivity {
 
+public class ActividadPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        Uri uri = Uri.parse("https://eventos-51c1f.firebaseapp.com/index.html");
+        intent.setData(uri);
+        startActivity(intent);
     }
+
 }
